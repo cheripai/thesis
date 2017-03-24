2fbe9b1793bb9fcc6974a4577c960049f916c970
dat <datdo@rocketmail.com>
2017-03-23 17:59:05 -0700

    Use data augmentation and change to keras 2.0

--- train_ndvi.py
+++ train_ndvi.py
@@ -1,7 +1,8 @@
 import bcolz
 import sys
+from keras.preprocessing.image import ImageDataGenerator
 from os import path
-from cnn import Inception
+from cnn import Inception, ResNet
 
 
 TRAIN_DATA = "data/train_ndvi/train.dat"
@@ -19,17 +20,31 @@ if __name__ == "__main__":
         train_target = bcolz.open(TRAIN_TARGET_DATA)[:]
         valid = bcolz.open(VALID_DATA)[:]
         valid_target = bcolz.open(VALID_TARGET_DATA)[:]
+        train = train.reshape(train.shape[0], train.shape[2], train.shape[3], train.shape[1])
+        valid = valid.reshape(valid.shape[0], valid.shape[2], valid.shape[3], valid.shape[1])
+        
+        datagen = ImageDataGenerator(
+            horizontal_flip=True,
+            width_shift_range=0.05,
+            height_shift_range=0.05,
+            zoom_range=0.05)
     else:
         print("No training or validation data found.")
         print("Run make_dataset.py first!")
         sys.exit(1)
 
-    cnn = Inception(1, 0.01, 0)
-    cnn.model.fit(
-        train,
-        train_target,
-        batch_size=64,
-        nb_epoch=100,
+    # cnn = Inception(1, 0.01, 0)
+    cnn = ResNet(1, 0.01, 0)
+    cnn.model.fit_generator(
+        datagen.flow(train, train_target, batch_size=64),
+        len(train),
+        epochs=100,
         validation_data=(valid, valid_target))
+   #  cnn.model.fit(
+   #      train,
+   #      train_target,
+   #      batch_size=64,
+   #      nb_epoch=100,
+   #      validation_data=(valid, valid_target))
 
     cnn.model.save_weights(WEIGHTS_PATH)
