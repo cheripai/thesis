8202cf8732b8fbef1d59992fe9d2c2eada053a9d
dat <datdo@rocketmail.com>
2017-08-12 11:15:58 -0700

    Add function to threshold low values to 0 for the purpose of removing the background from calculations

--- utils/gray2ndvi.py
+++ utils/gray2ndvi.py
@@ -6,12 +6,18 @@ import numpy as np
 import sys
 
 
+def threshold(arr, min_ndvi, max_ndvi, t=48):
+    step = (max_ndvi - min_ndvi) / 255
+    arr[arr <= t*step + min_ndvi] = 0
+    return arr
+
+
 def gray2ndvi(img, min_ndvi, max_ndvi):
     ndvi = np.zeros(gray.shape)
     step = np.zeros(gray.shape)
 
     step[:] = (max_ndvi - min_ndvi) / 255
-    ndvi = np.multiply(gray, step)
+    ndvi = np.multiply(gray, step) + min_ndvi
 
     return ndvi
 
@@ -22,5 +28,8 @@ if __name__ == "__main__":
 
     img = cv2.imread(img_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
+
     ndvi = gray2ndvi(gray, min_ndvi, max_ndvi)
+    ndvi = threshold(ndvi, min_ndvi, max_ndvi)
+
+    print(ndvi)
