f93d60c39d08a9ef41484a6b95c7579a5ef8060d
dat <datdo@rocketmail.com>
2017-09-16 15:00:09 -0700

    Determine rows and cols automatically

--- utils/stitcher/stitch.py
+++ utils/stitcher/stitch.py
@@ -1,8 +1,13 @@
+#
+# Takes the directory that contains subdirectories of the tiles
+#
+
 import numpy as np
 import os
 import re
 import scipy.misc
 import sys
+from PIL import Image
 from scipy import ndimage
 
 
@@ -15,13 +20,28 @@ def get_png_files(directory):
                 png_files.append(os.path.join(root, f))
     return png_files
 
+def get_size(directory):
+    for root, dirs, files in os.walk(directory):
+        for f in files:
+            if f.endswith(".png"):
+                img = Image.open(os.path.join(root, f))
+                return img.size[0]
+
+def get_rows_cols(directory):
+    for root, dirs, files in os.walk(directory):
+        cols = len(dirs)
+        # Divide by 2 because directory contains a png and kml file for each tile
+        rows = len([f for f in os.listdir(os.path.join(root, dirs[0]))]) // 2
+        return rows, cols
+    
 
 if __name__ == "__main__":
-    cols = 18
-    rows = 20
-    size = 256
+    directory = sys.argv[1]
+    size = get_size(directory)
+    rows, cols = get_rows_cols(directory)
+    print(size, rows, cols)
 
-    png_files = get_png_files(sys.argv[1])
+    png_files = get_png_files(directory)
     img = np.zeros((size * rows, size * cols, 3))
 
     k = 0
