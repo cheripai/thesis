8a1bd579e46f95b1567025d12d368c1d3cdfcde2
dat <datdo@rocketmail.com>
2017-11-15 18:41:27 -0800

    Use osgeo and handle transparency

--- utils/geotiff2img/convert.py
+++ utils/geotiff2img/convert.py
@@ -1,37 +1,39 @@
-import georasters as gr
 import numpy as np
 import os
 import pandas as pd
 import sys
+from osgeo import gdal
 from scipy.misc import imsave
 
 
 def strip_filename(fname):
     return os.path.splitext(os.path.basename(fname))[0]
 
-def geotiff2df(fname):
+def geotiff2arrays(fname):
     try:
-        data = gr.from_file(fname)
+        data = gdal.Open(fname)
     except:
         raise Exception("Invalid file!")
-    return data.to_pandas()
-
+    values = data.GetRasterBand(1).ReadAsArray()
+    transparency = data.GetRasterBand(2).ReadAsArray()
+    return values, transparency
 
 if __name__ == "__main__":
     infile = sys.argv[1]
-    df = geotiff2df(infile)
+    values, transparency = geotiff2arrays(infile)
 
-    min_value = df["value"].min()
-    max_value = df["value"].max()
+    min_value = values.min()
+    max_value = values.max()
 
     bins = np.zeros(256)
     for i in range(256):
         bins[i] = (max_value - min_value) / 256 * i + min_value
 
-    img_array = np.zeros((df["row"].max() + 1, df["col"].max() + 1))
-    for _, row in df.iterrows():
-        r, c = int(row["row"]), int(row["col"])
-        img_array[r, c] = np.argmin(np.abs(bins-row["value"]))
+    img_array = np.zeros((values.shape[0], values.shape[1], 4))
+    binner = lambda x: np.argmin(np.abs(bins-x))
+    vbin = np.vectorize(binner)
+    img_array[:, :, :3] = np.expand_dims(vbin(values), axis=-1)
+    img_array[:, :, 3] = transparency
 
     basename = strip_filename(infile)
     imsave(basename + ".png", img_array)
