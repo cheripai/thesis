0a9cffad8883a0668dccf9a414a4afee243acb7d
dat <datdo@rocketmail.com>
2017-12-12 19:52:43 -0800

    Use variables for paths

--- tiramisu/evaluate.py
+++ tiramisu/evaluate.py
@@ -7,20 +7,23 @@ from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
+X_PATH = "data/x_valid"
+Y_PATH = "data/y_valid"
+WEIGHTS_PATH = "data/weights.pth"
 
 if __name__ == "__main__":
-    eval_set = PairedImageLoader("data/x_valid", "data/y_valid", transform=transforms.ToTensor())
-    loader  = DataLoader(eval_set, batch_size=1, shuffle=False, num_workers=2, pin_memory=True)
+    eval_set = PairedImageLoader(X_PATH, Y_PATH, transform=transforms.ToTensor())
+    loader = DataLoader(eval_set, batch_size=1, shuffle=False, num_workers=2, pin_memory=True)
 
     model = FCDenseNet(4, 12, drop_rate=0.0, n_classes=1).cuda()
-    model.load_state_dict(torch.load("data/weights.pth"))
+    model.load_state_dict(torch.load(WEIGHTS_PATH))
     criterion = nn.L1Loss()
 
     total_loss = 0
     for j, (x, y) in enumerate(loader):
         x, y = Variable(x.cuda()), Variable(y.cuda())
         outputs = model(x)
-        loss = criterion(outputs*256, y*256)
+        loss = criterion(outputs * 256, y * 256)
         total_loss += loss.data[0]
 
-    print("Loss:", total_loss / (j+1))
+    print("MAE per pixel:", total_loss / (j + 1))
