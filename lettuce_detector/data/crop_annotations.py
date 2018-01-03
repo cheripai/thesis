484b78299086845164024a346d247a3a29d3a5dd
dat <datdo@rocketmail.com>
2018-01-03 15:53:18 -0800

    Add functionality for cropping from rows

--- lettuce_detector/data/crop_annotations.py
+++ lettuce_detector/data/crop_annotations.py
@@ -8,24 +8,31 @@ from scipy.misc import imsave
 output_dir = "cropped"
 
 if __name__ == "__main__":
-    img_path = sys.argv[1]
-    annotations_path = sys.argv[2]
+    annotations_path = sys.argv[1]
     row_order_path = None
 
-    if len(sys.argv) > 3:
-        row_order_path = sys.argv[3]
+    if len(sys.argv) > 2:
+        row_order_path = sys.argv[2]
         with open(row_order_path) as f:
             row_order = [row.strip() for row in f.readlines()]
-
-    img = np.asarray(Image.open(img_path), dtype=np.uint8)
-    annotations = json.load(open(annotations_path))[0]["annotations"]
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    for i, annotation in enumerate(annotations):
-        x, y, height, width = int(annotation["x"]), int(annotation["y"]), int(annotation["height"]), int(annotation["width"])
-        if row_order_path is not None:
+    annotations = json.load(open(annotations_path))
+
+    if row_order_path is not None:
+        img = np.asarray(Image.open(annotations[0]["filename"]), dtype=np.uint8)
+
+        for i, bbox in enumerate(annotations[0]["annotations"]):
+            x, y, height, width = int(bbox["x"]), int(bbox["y"]), int(bbox["height"]), int(bbox["width"])
             imsave(os.path.join(output_dir, row_order[i]+".png"), img[y:y+height, x:x+width])
-        else:
-            imsave(os.path.join(output_dir, str(i)+".png"), img[y:y+height, x:x+width])
+    else:
+        for annotation in annotations:
+            img = np.asarray(Image.open(annotation["filename"]), dtype=np.uint8)
+            bboxes = sorted(annotation["annotations"], key=lambda k: k["x"], reverse=True)
+            identifier, row = annotation["filename"].split("/")[-1].split(".")[0].split("Row")
+            row = int(row) * 100
+            for i, bbox in enumerate(bboxes):
+                x, y, height, width = int(bbox["x"]), int(bbox["y"]), int(bbox["height"]), int(bbox["width"])
+                imsave(os.path.join(output_dir, identifier+"_"+str(row+i+1)+".png"), img[y:y+height, x:x+width])
