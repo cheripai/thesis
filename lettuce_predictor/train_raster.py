76abcce1302ec01d61030193425494cd14428411
dat <datdo@rocketmail.com>
2018-01-30 16:29:54 -0800

    Shuffle set

--- lettuce_predictor/train_raster.py
+++ lettuce_predictor/train_raster.py
@@ -14,19 +14,19 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from torchvision.transforms.functional import hflip, vflip
 
-
 CLASSES = ["IR100", "IR50", "IR25", "IR0"]
+# CLASSES = ["N100", "N50", "N25", "N0"]
 CHANNELS = 5
 SIZE = 16
 
 
 class TreatmentDataset(Dataset):
     def __init__(self, img_paths, labels, mode="valid"):
-        
+
         self.img_paths = img_paths
         self.labels = labels
 
-        self.data_transforms = { 
+        self.data_transforms = {
             "train": transforms.Compose([
                 transforms.Resize(SIZE),
                 transforms.CenterCrop(SIZE),
@@ -53,7 +53,7 @@ class TreatmentDataset(Dataset):
         h_flip = random.random() < 0.5
         v_flip = random.random() < 0.5
         for i in range(CHANNELS):
-            channel = Image.fromarray(arr[:, :, i]*255).convert("L")
+            channel = Image.fromarray(arr[:, :, i] * 255).convert("L")
             if h_flip and self.mode == "train":
                 channel = hflip(channel)
             if v_flip and self.mode == "train":
@@ -69,13 +69,26 @@ def normalize(xs):
     max_x = max(xs)
     return [(x - min_x) / (max_x - min_x) for x in xs]
 
- 
+
+def get_train_test(root_dir, p=0.15):
+    img_paths = []
+    for img_dir in os.listdir(root_dir):
+        img_dir = os.path.join(root_dir, img_dir)
+        img_paths += [os.path.join(img_dir, name) for name in os.listdir(img_dir) if name.endswith("npy")]
+    # labels = [CLASSES.index(img_path.split("/")[-1].split("IR")[0]) for img_path in img_paths]
+    labels = [CLASSES.index("IR" + img_path.split("IR")[-1].split("Rep")[0]) for img_path in img_paths]
+    img_paths, labels = shuffle(img_paths, labels)
+    split = int(len(img_paths) * p)
+    return TreatmentDataset(img_paths[split:], labels[split:], "train"), TreatmentDataset(
+        img_paths[:split], labels[:split], "valid")
+
+
 def get_train_test_csv(csv_dir, target_col, p=0.15):
     img_paths = []
     labels = []
     for csv_path in os.listdir(csv_dir):
         if csv_path.endswith(".csv"):
-            with open(os.path.join(csv_dir,csv_path)) as f:
+            with open(os.path.join(csv_dir, csv_path)) as f:
                 reader = csv.DictReader(f)
                 for row in reader:
                     try:
@@ -83,28 +96,34 @@ def get_train_test_csv(csv_dir, target_col, p=0.15):
                         img_paths.append(row["img_path"])
                     except:
                         continue
+
+    img_paths, labels = shuffle(img_paths, labels)
+
     labels = normalize(labels)
     mean = np.mean(labels)
     labels = [int(label < mean) for label in labels]
+
     split = int(p * len(img_paths))
-    return TreatmentDataset(img_paths[split:], labels[split:], mode="train"), TreatmentDataset(img_paths[:split], labels[:split])
+    return TreatmentDataset(
+        img_paths[split:], labels[split:], mode="train"), TreatmentDataset(img_paths[:split], labels[:split])
 
 
 def correct(outputs, targets):
     _, outputs = torch.max(outputs.data, -1)
     return torch.sum(outputs == targets.data)
 
-        
+
 if __name__ == "__main__":
     batch_size = 32
     lr = 0.0001
-    leaf_train, leaf_valid = get_train_test_csv("data/rasters_csv", "Average Height")
-    print("Train Images:", len(leaf_train))
-    print("Valid Images:", len(leaf_valid))
-    train_loader = DataLoader(leaf_train, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
-    valid_loader = DataLoader(leaf_valid, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
+    train_set, valid_set = get_train_test_csv("data/rasters_csv", "WP")
+    # train_set, valid_set = get_train_test("data/rasters")
+    print("Train Images:", len(train_set))
+    print("Valid Images:", len(valid_set))
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    valid_loader = DataLoader(valid_set, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
 
-    model = RasterNet(len(CLASSES)).cuda()
+    model = RasterNet(2).cuda()
     criterion = nn.NLLLoss().cuda()
     optimizer = optim.Adam(model.parameters(), lr=lr)
     scheduler = ReduceLROnPlateau(optimizer, "min", factor=0.5, patience=5)
@@ -112,11 +131,13 @@ if __name__ == "__main__":
     print("Learning rate: {}".format(lr))
 
     epochs = 100
+    top_acc = 0
     for i in range(epochs):
         print("Epoch", i)
         train_loss = 0
         train_acc = 0
         count = 0
+        model.train(True)
         for X, y in train_loader:
             X, y = Variable(X.cuda()), Variable(y.type(torch.LongTensor).cuda())
             optimizer.zero_grad()
@@ -125,7 +146,7 @@ if __name__ == "__main__":
             loss.backward()
             optimizer.step()
             train_loss += loss.data[0]
-            train_acc += correct(outputs ,y)
+            train_acc += correct(outputs, y)
             count += X.size(0)
         train_acc /= count
         scheduler.step(train_loss)
@@ -134,12 +155,16 @@ if __name__ == "__main__":
         valid_loss = 0
         valid_acc = 0
         count = 0
+        model.train(False)
         for X, y in valid_loader:
             X, y = Variable(X.cuda()), Variable(y.type(torch.LongTensor).cuda())
             outputs = model(X)
             loss = criterion(outputs, y)
             valid_loss += loss.data[0]
-            valid_acc += correct(outputs ,y)
+            valid_acc += correct(outputs, y)
             count += X.size(0)
         valid_acc /= count
+        if valid_acc > top_acc:
+            top_acc = valid_acc
         print("Valid Loss {} Valid Accuracy {}".format(round(valid_loss, 4), round(valid_acc, 4)))
+    print("Top Accuracy {}".format(round(top_acc, 4)))
