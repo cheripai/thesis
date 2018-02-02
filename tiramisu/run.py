95e1dea38f105bfc647b329045eb97414db4fcbc
dat <datdo@rocketmail.com>
2018-02-01 17:43:29 -0800

    Add zero padding

--- tiramisu/run.py
+++ tiramisu/run.py
@@ -14,8 +14,8 @@ from torchvision import transforms
 
 
 def flatten_to_tiles(img, tile_size=c.IMG_SIZE):
-    h, w, c = img.shape
-    tiles = np.zeros(((h // tile_size) * (w // tile_size), tile_size, tile_size, c))
+    h, w, ch = img.shape
+    tiles = np.zeros(((h // tile_size) * (w // tile_size), tile_size, tile_size, ch))
     for i in range(h // tile_size - 1):
         y1, y2 = i * tile_size, (i+1) * tile_size
         for j in range(w // tile_size - 1):
@@ -37,9 +37,11 @@ def unflatten_tiles(tiles, h, w):
 
 if __name__ == "__main__":
     img = np.asarray(Image.open(sys.argv[1]), dtype=np.uint8)
-    h, w = img.shape[:2]
+    h, w, ch = img.shape
 
-    # TODO: zero padding
+    padded_img = np.zeros((h + 2*c.IMG_SIZE, w + 2*c.IMG_SIZE, ch))
+    padded_img[:h, :w, :] = img
+    p_h, p_w, _ = padded_img.shape
 
     model = FCDenseNet([4, 5, 7, 10, 12, 15, 12, 10, 7, 5, 4], 16, drop_rate=0.0, n_classes=1).cuda()
     model.load_state_dict(torch.load(c.WEIGHTS_PATH))
@@ -47,7 +49,8 @@ if __name__ == "__main__":
 
     output_map = np.zeros((h, w))
 
-    in_tiles = np.moveaxis(flatten_to_tiles(img, c.IMG_SIZE), -1, 1)
+    in_tiles = np.moveaxis(flatten_to_tiles(padded_img, c.IMG_SIZE), -1, 1)
+    imsave("test.png", unflatten_tiles(in_tiles[:, 0], p_h, p_w)[:h, :w])
     out_tiles = np.zeros((in_tiles.shape[0], c.IMG_SIZE, c.IMG_SIZE))
     for i in range(in_tiles.shape[0] // c.BATCH_SIZE):
         if (i+1) * c.BATCH_SIZE > in_tiles.shape[0]:
@@ -59,5 +62,5 @@ if __name__ == "__main__":
         out_segment = model(in_segment).data.squeeze().cpu().numpy()
         out_tiles[i*c.BATCH_SIZE:(i+1)*batch_size] = out_segment
 
-    out_img = unflatten_tiles(out_tiles, h, w)
-    imsave("output.png", out_img)
+    out_img = unflatten_tiles(out_tiles, p_h, p_w)
+    imsave("output.png", out_img[:h, :w])
