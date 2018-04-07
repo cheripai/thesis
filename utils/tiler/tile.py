48b8fdbb3637395f2fbcdadbeebdad8d5c809a78
dat <datdo@rocketmail.com>
2018-04-06 17:59:01 -0700

    Tile all of image

--- utils/tiler/tile.py
+++ utils/tiler/tile.py
@@ -13,17 +13,16 @@ from random import randint
 from scipy.misc import imsave
 from sklearn.feature_extraction.image import extract_patches_2d
 
-patch_size = 256
+patch_size = 56
 
 if __name__ == "__main__":
-    seed = randint(0, 1000)
     for i in range(len(sys.argv) - 1):
         dir_name = "tiles_{}".format(i)
         if not os.path.exists(dir_name):
             os.makedirs(dir_name)
         img = np.asarray(Image.open(sys.argv[i + 1]), dtype=np.uint8)
         h, w = img.shape[:2]
-        max_patches = int(1.5 * h * w / patch_size**2)
-        patches = extract_patches_2d(img, patch_size=(patch_size, patch_size), max_patches=max_patches, random_state=seed)
-        for j in range(patches.shape[0]):
-            imsave("{}/{}.png".format(dir_name, j), patches[j])
+
+        for j in range((h // patch_size) - 1):
+            for k in range((w // patch_size) - 1):
+                imsave("{}/{}_{}.png".format(dir_name, j, k), img[patch_size*j:patch_size*(j+1), patch_size*k:patch_size*(k+1)])
